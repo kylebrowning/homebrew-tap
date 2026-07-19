131e927699dc8c657661ca863ff37247512cd4fe
Kyle Browning <kylebrowning@me.com>
2026-07-19 08:17:47 -0700

    ble: update sha256 for re-cut 1.0.0 tag (now includes LICENSE)

--- Formula/ble.rb
+++ Formula/ble.rb
@@ -2,7 +2,7 @@ class Ble < Formula
   desc "Bluetooth LE CLI for macOS: scan, connect, pair, read, write, inspect, L2CAP"
   homepage "https://github.com/kylebrowning/BLESwiftCLI"
   url "https://github.com/kylebrowning/BLESwiftCLI/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "5df25f087b123f49582dcb4c5e7064ac310d337eeeb44cf9ab0302e9509f46bc"
+  sha256 "39ea024ff76f92ad200ab02854ee6a0f6ce1183a2e8b7803029128ef7b5f22bf"
   license "Apache-2.0"
   head "https://github.com/kylebrowning/BLESwiftCLI.git", branch: "main"
 
